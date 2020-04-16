d5972635fdb74866a02bd5e65c5cb760bb8a49cb
Saksham <chauhanssaksham@gmail.com>
2020-04-16 05:52:43 +0530

    Added sign in and sign up views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,11 @@
 module.exports.profile = (req,res)=>{
     return res.render('user_profile')
 }
+
+module.exports.signIn = (req,res)=>{
+    return res.render('user_sign_in')
+}
+
+module.exports.signUp = (req,res)=>{
+    return res.render('user_sign_up')
+}
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 const usersController = require('../controllers/users_controller')
 
-router.use('/', usersController.profile)
+router.get('/', usersController.profile);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 module.exports = router;
