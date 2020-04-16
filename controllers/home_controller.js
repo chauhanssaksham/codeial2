3be498fd9a8a156c8e238ce638a040848a4aa966
Saksham <chauhanssaksham@gmail.com>
2020-04-16 13:03:07 +0530

    Showing comments on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,15 @@
 const Post = require('../models/post')
 
 module.exports.home = (req,res) => {
-    Post.find({}).populate('user').exec((err,posts)=>{
+    Post.find({})
+    .populate('user')
+    .populate(({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    }))
+    .exec((err,posts)=>{
         return res.render('home', {
             posts: posts
         })
