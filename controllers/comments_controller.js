de13e922fec06c8a047ccdfa0ba7cf1b6798637e
Saksham <chauhanssaksham@gmail.com>
2020-04-16 23:57:31 +0530

    Added DELETE Comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,3 +24,26 @@ module.exports.create = (req,res) => {
         }
     })
 }
+
+module.exports.destroy = (req,res) => {
+    Comment.findById(req.params.id).populate('post').exec((err,comment)=>{
+        if (err){
+            console.log("Comment not found");
+            return res.redirect('back');
+        }
+        if (comment){
+            if (comment.user == req.user.id || comment.post.user == req.user.id){
+                let post_id = comment.post;
+                comment.remove();
+                Post.findByIdAndUpdate(post_id, {$pull: {comments: req.params.id}}, (err, post)=>{
+                    if (err){
+                        return res.redirect('back');
+                    }
+                    return res.redirect('back');
+                });
+            }
+        } else {
+        return res.redirect('back');
+        }
+    })
+}
