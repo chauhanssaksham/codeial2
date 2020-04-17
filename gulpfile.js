8d9a06cf87bcb15280a1f98b84e915967c3eb4e2
Saksham <chauhanssaksham@gmail.com>
2020-04-18 04:55:55 +0530

    Added GulpFile for building production build

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+
+function css(done){
+    console.log('Converting Scss to css...');
+    gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(gulp.dest('./assets/css'));
+
+    console.log('Minifying CSS files and placing into public folder')
+    return gulp.src('./assets/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd:'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+
+    done();
+}
+
+function js(done){
+    console.log('minifying js...');
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+}
+
+function minifyImages(done){
+    console.log('compressing images...');
+    return gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+}
+
+function clean(done){
+    console.log("Cleaning assets...");
+    del.sync('./public/assets');
+    done();
+}
+
+module.exports.build = gulp.series(clean, css, js, minifyImages);
