62b6b614640b35922a80663919489a54dd7532fc
Saksham <chauhanssaksham@gmail.com>
2020-04-18 13:42:00 +0530

    Completed users API

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -26,3 +26,68 @@ module.exports.createSession = async (req, res) => {
         })
     }
 }
+
+module.exports.create = async (req,res)=>{
+    try {
+        if(req.body.password != req.body.confirm_password){
+            return res.status(401).json({
+                success:false,
+                message: "Password don't match"
+            })
+        }
+        let user = await User.findOne({email: req.body.email})
+        if (!user){
+            let newUser = await User.create(req.body)
+            return res.status(200).json({
+                success:true,
+                message: "New user created, please sign in"
+            })
+        } else{
+            res.status(401).json({
+                success:false,
+                message: "User already exists"
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: "Internal server error"
+        })
+    }
+}
+
+module.exports.update = async (req,res) => {
+    if (req.user.id == req.params.id){
+        try {
+            let user = await User.findById(req.params.id)
+            User.uploadedAvatar(req, res, function(err){
+                if (err){console.log("***Multer Error", err);}
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if (req.file){  //If a file is also uploaded
+                    if(user.avatar && fs.existsSync(path.join(__dirname, '..', user.avatar))){    //User has avatar field, and that file exists in the system
+                        fs.unlinkSync(path.join(__dirname, '..', user.avatar))  //Delete the previous image
+                    }
+                    //Save the path of the uploaded file into user.avatar
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+            })
+            return res.status(200).json({
+                message:"Updated successfully",
+                success:true
+            })
+        } catch (error) {
+            return res.status(401).json({
+                success:false,
+                message:"You are unauthorized to do that"
+            })
+        }
+    } else {
+        return res.status(403).json({
+            success:false,
+            message:"Unauthorized"
+        });
+    }
+}
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const usersApiController = require('../../../controllers/api/v1/users_api')
+const passport = require('passport')
 
 //@route        POST /api/v1/users/login
 //@description  Log in a user
 //@access       Public
 //@Required     req.body fields: email and password
 router.post('/login', usersApiController.createSession)
+
+//@route        POST /api/v1/users/create
+//@description  Create/signup a user
+//@access       Public
+//@Required     req.body fields: name, email and password & confirm_password
+router.post('/create', usersApiController.create)
+
+//@route        POST /api/v1/users/update/:id
+//@description  Update user info, add/change profile pic
+//@access       Private
+//@Required     req.body fields: name, email, avatar (containing photo)
+router.post('/update/:id', passport.authenticate('jwt', {session:false}), usersApiController.update)
+
 
 module.exports = router;
