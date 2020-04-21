ed0c555e6fac11d1c62f74e1cee8743d6a81a14c
Saksham <chauhanssaksham@gmail.com>
2020-04-21 08:21:04 +0530

    Connection between user and obeserver established

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,16 @@
 module.exports.chatSockets = (socketServer)=>{
+    let io = require('socket.io')(socketServer)
+
+    //Event is different just in this case
+    //Udhar connect hai, idhar connection
+    io.sockets.on('connection', (socket)=>{
+        console.log("new connection recieved :", socket.id)
+
+        socket.on('disconnect', ()=>{
+            console.log("Socket disconnected!");
+        })
+
+    })
+
 
 }
